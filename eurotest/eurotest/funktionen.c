c06c41288e57e840608c1e546ccd8f7e3e1f00fc
Carolime <ariesu@outlook.de>
2018-10-06 19:39:40 +0200

    Init

---
+++ eurotest/eurotest/funktionen.c
@@ -0,0 +1,21 @@
+/*******************************************************************
+// Programm    : Eurotest                                               
+// Verfasser   : Schmidt                                             
+// Datum       : Urprogramm: 21.11.2012                                           
+// Aufgabe     : Prüfung von Euro-Banknoten Seriennummern
+// Änderungen  : 21.11.2012
+*******************************************************************/
+
+#include <stdio.h>  /* Standard Input/Output  z.B. scanf, printf */
+#include "funktionen.h"
+
+
+
+void strich (int n, char c) // gibt n mal das Zeichen c aus
+{
+	int i;
+	for (i = 1; i <= n; i++)
+		printf("%c", c);
+	printf("\n");
+}
+
---
+++ eurotest/eurotest/mainfile.c
@@ -0,0 +1,23 @@
+/*******************************************************************
+// Programm    : Eurotest                                               
+// Verfasser   : Schmidt                                             
+// Datum       : Urprogramm: 21.11.2012                                           
+// Aufgabe     : Prüfung von Euro-Banknoten Seriennummern
+// Änderungen  : 21.11.2012
+*******************************************************************/
+
+#include <stdio.h>  /* Standard Input/Output  z.B. scanf, printf */
+#include <stdlib.h>   /* Standard-Bibliothek, z.B. für system       */
+#include "funktionen.h"
+
+
+int main () 
+{
+	strich(50,'-');
+	printf("Eurotest\n");
+	strich(50,'-');
+
+	printf("\nServus! \n");
+	system("pause");
+	return 0;
+}
